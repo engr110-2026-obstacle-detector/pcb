f8a7f4a54dc731da96298d8ba20904cc4d735d77
joshua-8 <joshua262144@gmail.com>
2026-03-20 02:07:33 -0700

    working

---
+++ script.py
@@ -0,0 +1,12 @@
+import os
+import shutil
+import kikit
+
+scratchFolderDir=os.getcwd()+"front/
+
+print("File location using os.getcwd():", 
+      os.getcwd())
+
+
+#board = LoadBoard()
+#panel = Panel(output)
