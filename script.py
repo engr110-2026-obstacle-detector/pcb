a38028e0e660f029bce3cb0f2c7b29a224463e8d
joshua-8 <joshua262144@gmail.com>
2026-03-21 13:16:13 -0700

    undo panelizing

--- script.py
+++ script.py
@@ -8,6 +8,15 @@ from pcbnew import *
 from kikit.common import fromMm
 from shapely.geometry import box
 
+def makeGerbers(inName, outName):
+    subprocess.run(["kicad-cli", "pcb", "export", "gerbers", "--layers", "F.Cu,B.Cu,F.Paste,B.Paste,F.Silkscreen,B.Silkscreen,F.Mask,B.Mask,Edge.Cuts", os.getcwd()+"/front/scratch/"+inName, "--output", os.getcwd()+"/front/scratch/"+outName])
+    subprocess.run(["kicad-cli", "pcb", "export", "drill", os.getcwd()+"/front/scratch/"+inName, "--output", os.getcwd()+"/front/scratch/"+outName])
+
+    shutil.make_archive(os.getcwd()+"/front/scratch/"+outName, 'zip', os.getcwd()+"/front/scratch/"+outName)
+    shutil.rmtree(os.getcwd()+"/front/scratch/"+outName)
+
+    shutil.move(os.getcwd()+"/front/scratch/"+outName+".zip", os.getcwd()+"/front/PCB_Production/"+outName+".zip")
+
 print("starting script ...")
 
 pcbnew.KIID.SeedGenerator(42) # deterministic
@@ -29,18 +38,15 @@ os.system('kikit separate --source "rectangle; tlx: 33mm; tly: 15mm; brx: 126mm;
 os.system('kikit separate --source "rectangle; tlx: 33mm; tly: 75mm; brx: 125mm; bry: 132mm" front/front.kicad_pcb front/scratch/front_sensor_L.kicad_pcb')
 os.system('kikit separate --source "rectangle; tlx: 33mm; tly: 75mm; brx: 125mm; bry: 132mm" front/front.kicad_pcb front/scratch/front_sensor_C.kicad_pcb')
 os.system('kikit separate --source "rectangle; tlx: 33mm; tly: 75mm; brx: 125mm; bry: 132mm" front/front.kicad_pcb front/scratch/front_sensor_R.kicad_pcb')
-os.system('kikit separate --source "rectangle; tlx: 33mm; tly: 136mm; brx: 125mm; bry: 195mm" front/front.kicad_pcb front/scratch/front_bottom.kicad_pcb') 
 os.system('kikit separate --source "rectangle; tlx: 154mm; tly: 43mm; brx: 273mm; bry: 141mm" front/front.kicad_pcb front/scratch/front_back.kicad_pcb')
 
 print("exporting pieces to 3d models ...")
 subprocess.run(['kicad-cli', 'pcb', 'export', 'step', 'front/scratch/front_top.kicad_pcb', '-o', 'front/3d_models/front_top.step'], stdout=subprocess.DEVNULL)
 subprocess.run(['kicad-cli', 'pcb', 'export', 'step', 'front/scratch/front_sensor_C.kicad_pcb', '-o', 'front/3d_models/front_sensor.step'], stdout=subprocess.DEVNULL)
-subprocess.run(['kicad-cli', 'pcb', 'export', 'step', 'front/scratch/front_bottom.kicad_pcb', '-o', 'front/3d_models/front_bottom.step'], stdout=subprocess.DEVNULL)
 subprocess.run(['kicad-cli', 'pcb', 'export', 'step', 'front/scratch/front_back.kicad_pcb', '-o', 'front/3d_models/front_back.step'], stdout=subprocess.DEVNULL)
 
 subprocess.run(['kicad-cli', 'pcb', 'export', 'stl', 'front/scratch/front_top.kicad_pcb', '-o', 'front/3d_models/front_top.stl'], stdout=subprocess.DEVNULL)
 subprocess.run(['kicad-cli', 'pcb', 'export', 'stl', 'front/scratch/front_sensor_C.kicad_pcb', '-o', 'front/3d_models/front_sensor.stl'], stdout=subprocess.DEVNULL)
-subprocess.run(['kicad-cli', 'pcb', 'export', 'stl', 'front/scratch/front_bottom.kicad_pcb', '-o', 'front/3d_models/front_bottom.stl'], stdout=subprocess.DEVNULL)
 subprocess.run(['kicad-cli', 'pcb', 'export', 'stl', 'front/scratch/front_back.kicad_pcb', '-o', 'front/3d_models/front_back.stl'], stdout=subprocess.DEVNULL)
 
 print("panelizing ...")
@@ -64,55 +70,27 @@ panel.appendBoard(os.getcwd()+"/front/scratch/front_sensor_C.kicad_pcb", destina
 destinationLocation = wxPointMM(150+20, sensorY)
 panel.appendBoard(os.getcwd()+"/front/scratch/front_sensor_R.kicad_pcb", destinationLocation, fullPageArea, inheritDrc=False)
 
-destinationLocation = wxPointMM(265, 80)
-panel.appendBoard(os.getcwd()+"/front/scratch/front_bottom.kicad_pcb", destinationLocation, fullPageArea, inheritDrc=False, rotationAngle=EDA_ANGLE(90, pcbnew.DEGREES_T))
-
 destinationLocation = wxPointMM(150, 182)
 panel.appendBoard(os.getcwd()+"/front/scratch/front_back.kicad_pcb", destinationLocation, fullPageArea, inheritDrc=False)
-
-frame=panel.makeTightFrame(fromMm(4), fromMm(4), fromMm(2.1), fromMm(2.1), fromMm(2.1))
-
-for s in panel.substrates:
-    expansion = fromMm(2.2)
-    minx, miny, maxx, maxy = s.bounds()
-    bbox_box = box(minx-expansion, miny-expansion, maxx+expansion, maxy+expansion)
-    s.partitionLine = bbox_box.exterior
-
-# panel.debugRenderPartitionLines()
-
-tabCuts = panel.buildTabsFromAnnotations(fillet=fromMm(0))
-
-# render cuts from lines to mouse bits
-panel.makeMouseBites(
-    cuts=tabCuts,
-    diameter=fromMm(0.3),      # Drill diameter for mouse bites in mm
-    spacing=fromMm(0.475),         # Spacing between individual bites in mm
-    offset=fromMm(0.0),       # Offset from partition line in mm
-    prolongation=fromMm(.6)   # How much to extend the cut in mm
-)
-
-panel.addMillFillets(fromMm(1.025))
 
 panel.save()
 
 print("panel saved.")
 
+subprocess.run(['kicad-cli', 'pcb', 'export', 'stl', '--board-only', 'front/scratch/PANEL.kicad_pcb', '-o', 'front/3d_models/stencil.stl'], stdout=subprocess.DEVNULL)
+subprocess.run(['kicad-cli', 'pcb', 'export', 'step', '--board-only', 'front/scratch/PANEL.kicad_pcb', '-o', 'front/3d_models/stencil.step'], stdout=subprocess.DEVNULL)
+
 print("exporting gerbers ...")
-subprocess.run(["kicad-cli", "pcb", "export", "gerbers", "--layers", "F.Cu,B.Cu,F.Paste,B.Paste,F.Silkscreen,B.Silkscreen,F.Mask,B.Mask,Edge.Cuts", os.getcwd()+"/front/scratch/PANEL.kicad_pcb", "--output", os.getcwd()+"/front/scratch/gerbers"])
-subprocess.run(["kicad-cli", "pcb", "export", "drill", os.getcwd()+"/front/scratch/PANEL.kicad_pcb", "--output", os.getcwd()+"/front/scratch/gerbers"])
-
-
-shutil.make_archive(os.getcwd()+"/front/scratch/gerbers", 'zip', os.getcwd()+"/front/scratch/gerbers")
-shutil.rmtree(os.getcwd()+"/front/scratch/gerbers")
-
-shutil.move(os.getcwd()+"/front/scratch/gerbers.zip", os.getcwd()+"/front/PCB_Production/gerbers.zip")
+makeGerbers("PANEL.kicad_pcb", "stencil_gerbers")
+makeGerbers("front_top.kicad_pcb", "front_top_gerbers")
+makeGerbers("front_sensor_C.kicad_pcb", "front_sensor_gerbers")
+makeGerbers("front_back.kicad_pcb", "front_back_gerbers")
 
 print("rendering images ...")
 subprocess.run(["kicad-cli", "pcb", "render", os.getcwd()+"/front/scratch/PANEL.kicad_pcb", "--output", os.getcwd()+"/front/renders/panel.png"])
 
 subprocess.run(["kicad-cli", "pcb", "render", os.getcwd()+"/front/scratch/front_top.kicad_pcb", "--output", os.getcwd()+"/front/renders/front_top.png"])
 subprocess.run(["kicad-cli", "pcb", "render", os.getcwd()+"/front/scratch/front_sensor_C.kicad_pcb", "--output", os.getcwd()+"/front/renders/front_sensor_C.png"])
-subprocess.run(["kicad-cli", "pcb", "render", os.getcwd()+"/front/scratch/front_bottom.kicad_pcb", "--output", os.getcwd()+"/front/renders/front_bottom.png"])
 subprocess.run(["kicad-cli", "pcb", "render", os.getcwd()+"/front/scratch/front_back.kicad_pcb", "--output", os.getcwd()+"/front/renders/front_back.png"])
 
 
